1001027b3b876834c8065ee6c12679f7422317ec
Belmetiuk Joaquin <belme.joa@gmail.com>
2023-03-19 21:33:04 -0300

    add fuctions in js and more structure html

--- apps/app.js
+++ apps/app.js
@@ -1,28 +1,44 @@
+//Saludo a diosito
+alert("Hola buenas! Con esta app la idea es ayudarte con el juego y que no te explote la calabaza asi que lets go!")
+//Cantidad
 let nombres = []
-alert("Bienvenido al juego de la Mafia antes de entrar tienes q ingresar el nro de participantes y sus nombres.Graciass!")
-let ingresarJugadores = () => {
-    let cantidad = Number(prompt("Ingrese la cantidad de jugadores"))
-    if (cantidad <= 5) {
-        alert("Necesitas al menos 6 o mas personas para jugar o ingresa correctamente el numero de jugadores")
-    } else {
-        for (let i = 1; i <= cantidad; i++) {
-            let nombre = prompt("Ingrese nombres de a uno").toLowerCase()
-            nombres.push(nombre)
-        } return nombres
+let cantidad
+const ingresarCantidad = ()=>{
+    cantidad =  prompt("Ingresa la cantidad de jugadores")
+    if (cantidad <= 5 || cantidad=="" ) {
+        document.getElementById("mensaje").innerHTML= "Necesitas al menos 6 personas para poder jugar. Ingresa la cantidad nuevamente"
     }
+    nombres.length = cantidad 
+    return cantidad
 }
-ingresarJugadores()
-console.log(nombres)
+console.log(cantidad)
+let ingresarJugadores = (cantidad) => {
+    if(cantidad>=6){
+        for (let i = 1; i <= cantidad; i++) {
+            let nombre = prompt(`Ingrese nombre del jugador ${i}`).toLowerCase()
+            nombres.splice((i-1),1,nombre)
+            console.log(nombres)
+    } }
+  //add listado en el html
+    for (let i = 1; i <= cantidad; i++) {
+        document.getElementById("jugadores").innerHTML += `   ${(i)}) ${nombres[i-1]}   `
+    } 
+}
+//nro de carta de cada
+let cartas=[]
+let ingresarCarta=()=>{
+    for (let i = 1; i <= nombres.length; i++) {
+        let carta = Number(prompt(`Ingrese numero de la carta del jugador ${i} ${nombres[i]}`))
+        cartas.push(carta)
+    }console.log(cartas)
+}
 
-for (let i = 0; i <= nombres.length - 1; i++) {
-    document.getElementById("jugadores").innerHTML += `   ${i + 1}) ${nombres[i]}   `
-}
 //TEMPORIZADOR
-let segundos = 120;
+let segundos = 5;
 const actualizarTiempo=()=>{
 	document.getElementById("segundos").innerHTML = segundos;
 	if(segundos==0){
-		alert("El tiempo se acabo..");
+        alert("El tiempo se acabo..");
         segundos=120
 	}else{
 		segundos--;
